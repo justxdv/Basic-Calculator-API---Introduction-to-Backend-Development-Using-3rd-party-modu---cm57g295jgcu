027215d1dbc680884b9712851a145f082c1eb415
Digvijay Sethi <59555227+justxdv@users.noreply.github.com>
2023-03-19 16:21:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.post('/add', function(req, res){
     const num1 = parseFloat(req.body.num1);
     const num2 = parseFloat(req.body.num2);
     if(isNaN(num1) || isNaN(num2)){
-        return res.status(400).json({error: "Invalid input. Please provide two numbers."});
+        return res.status(400).json({error: "Invalid input. Please provide two valid numbers."});
     }
     const result = num1 + num2;
     res.status(200).json({result: "The sum of given two numbers", value: result});
@@ -25,7 +25,7 @@ app.post('/sub', function(req, res){
     const num1 = parseFloat(req.body.num1);
     const num2 = parseFloat(req.body.num2);
     if(isNaN(num1) || isNaN(num2)){
-        return res.status(400).json({error: "Invalid input. Please provide two numbers."});
+        return res.status(400).json({error: "Invalid input. Please provide two valid numbers."});
     }
     const result = num1 - num2;
     res.status(200).json({result: "The difference of given two numbers", value: result});
@@ -35,7 +35,7 @@ app.post('/multiply', function(req, res){
     const num1 = parseFloat(req.body.num1);
     const num2 = parseFloat(req.body.num2);
     if(isNaN(num1) || isNaN(num2)){
-        return res.status(400).json({error: "Invalid input. Please provide two numbers."});
+        return res.status(400).json({error: "Invalid input. Please provide two valid numbers."});
     }
     const result = num1 * num2;
     res.status(200).json({result: "The multiplication of given two numbers", value: result});
@@ -45,13 +45,26 @@ app.post('/divide', function(req, res){
     const num1 = parseFloat(req.body.num1);
     const num2 = parseFloat(req.body.num2);
     if(isNaN(num1) || isNaN(num2)){
-        return res.status(400).json({error: "Invalid input. Please provide two numbers."});
+        return res.status(400).json({error: "Invalid input. Please provide two valid numbers."});
     }
     if(num2 === 0){
         return res.status(400).json({error: "Invalid input. Cannot divide by zero."});
     }
     const result = num1 / num2;
-    res.status(200).json({result: "The division of given two numbers", value: result});
+    if(isFinite(result)){
+        res.status(200).json({result: "The division of given two numbers", value: result});
+    }else{
+        res.status(400).json({error: "Invalid input. Result is not a finite number."});
+    }
+});
+
+app.use((req, res) => {
+  res.status(404).json({error: "Invalid URL."});
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({error: "Internal Server Error."});
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
